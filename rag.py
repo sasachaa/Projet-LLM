8e7a75e46f46e1d7b503d02a59fc66e988347a61
victoriatroubat <victoria_troubat@hotmail.fr>
2025-03-26 21:34:24 +0100

    Add files via upload

--- rag.py
+++ rag.py
@@ -2,3 +2,19 @@ dataset = []
 with open('cat-facts.txt', 'r') as file:
     dataset = file.readlines()
     print(f'Loaded {len(dataset)} entries')
+
+import ollama
+
+EMBEDDING_MODEL = 'hf.co/CompendiumLabs/bge-base-en-v1.5-gguf'
+LANGUAGE_MODEL = 'hf.co/bartowski/Llama-3.2-1B-Instruct-GGUF'
+
+# Each element in the VECTOR_DB will be a tuple (chunk, embedding)
+# The embedding is a list of floats, for example: [0.1, 0.04, -0.34, 0.21, ...]
+VECTOR_DB = []
+
+def add_chunk_to_database(chunk):
+    embedding = ollama.embed(model=EMBEDDING_MODEL, input=chunk)['embeddings'][0]
+    VECTOR_DB.append((chunk, embedding))
+    for i, chunk in enumerate(dataset):
+        add_chunk_to_database(chunk)
+        print(f'Added chunk {i+1}/{len(dataset)} to the database')
