f641a7a58cc8cfbd822b65f9c7eb6dbc658ba33b
victoriatroubat <victoria_troubat@hotmail.fr>
2025-03-26 21:45:52 +0100

    Add files via upload

--- rag.py
+++ rag.py
@@ -5,8 +5,8 @@ import ollama
 
 dataset = []
 with open('cat-facts.txt', 'r') as file:
-  dataset = file.readlines()
-  print(f'Loaded {len(dataset)} entries')
+    dataset = file.readlines()
+    print(f'Loaded {len(dataset)} entries')
 
 
 
@@ -20,12 +20,12 @@ LANGUAGE_MODEL = 'hf.co/bartowski/Llama-3.2-1B-Instruct-GGUF'
 VECTOR_DB = []
 
 def add_chunk_to_database(chunk):
-  embedding = ollama.embed(model=EMBEDDING_MODEL, input=chunk)['embeddings'][0]
-  VECTOR_DB.append((chunk, embedding))
+    embedding = ollama.embed(model=EMBEDDING_MODEL, input=chunk)['embeddings'][0]
+    VECTOR_DB.append((chunk, embedding))
 
 for i, chunk in enumerate(dataset):
-  add_chunk_to_database(chunk)
-  print(f'Added chunk {i+1}/{len(dataset)} to the database')
+    add_chunk_to_database(chunk)
+    print(f'Added chunk {i+1}/{len(dataset)} to the database')
 
 def cosine_similarity(a, b):
   dot_product = sum([x * y for x, y in zip(a, b)])
@@ -34,16 +34,16 @@ def cosine_similarity(a, b):
   return dot_product / (norm_a * norm_b)
 
 def retrieve(query, top_n=3):
-  query_embedding = ollama.embed(model=EMBEDDING_MODEL, input=query)['embeddings'][0]
-  # temporary list to store (chunk, similarity) pairs
-  similarities = []
-  for chunk, embedding in VECTOR_DB:
-    similarity = cosine_similarity(query_embedding, embedding)
-    similarities.append((chunk, similarity))
-  # sort by similarity in descending order, because higher similarity means more relevant chunks
-  similarities.sort(key=lambda x: x[1], reverse=True)
-  # finally, return the top N most relevant chunks
-  return similarities[:top_n]
+    query_embedding = ollama.embed(model=EMBEDDING_MODEL, input=query)['embeddings'][0]
+    # temporary list to store (chunk, similarity) pairs
+    similarities = []
+    for chunk, embedding in VECTOR_DB:
+        similarity = cosine_similarity(query_embedding, embedding)
+        similarities.append((chunk, similarity))
+    # sort by similarity in descending order, because higher similarity means more relevant chunks
+    similarities.sort(key=lambda x: x[1], reverse=True)
+    # finally, return the top N most relevant chunks
+    return similarities[:top_n]
 
 
 
@@ -54,7 +54,7 @@ retrieved_knowledge = retrieve(input_query)
 
 print('Retrieved knowledge:')
 for chunk, similarity in retrieved_knowledge:
-  print(f' - (similarity: {similarity:.2f}) {chunk}')
+    print(f' - (similarity: {similarity:.2f}) {chunk}')
 
 instruction_prompt = f'''You are a helpful chatbot.
 Use only the following pieces of context to answer the question. Don't make up any new information:
@@ -63,15 +63,15 @@ Use only the following pieces of context to answer the question. Don't make up a
 # print(instruction_prompt)
 
 stream = ollama.chat(
-  model=LANGUAGE_MODEL,
-  messages=[
+    model=LANGUAGE_MODEL,
+    messages=[
     {'role': 'system', 'content': instruction_prompt},
     {'role': 'user', 'content': input_query},
-  ],
-  stream=True,
+    ],
+    stream=True,
 )
 
 # print the response from the chatbot in real-time
 print('Chatbot response:')
 for chunk in stream:
-  print(chunk['message']['content'], end='', flush=True)
+    print(chunk['message']['content'], end='', flush=True)
