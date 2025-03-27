10f92056d08bfc63ab28f73e7fe1bafb3d29cdd0
sasachaa <sarahchaab007@gmail.com>
2025-03-27 17:29:49 +0100

    commentaire pour demain

--- ragSarah.py
+++ ragSarah.py
@@ -47,7 +47,7 @@ def get_embeddings(sentences, model_name="sentence-transformers/all-MiniLM-L6-v2
 
 sentences = [dataset]
 
-
+## changer add_chunk_to_database dans la oucle car notre pb c'est qu'il prend seulement dans l'embedding la première phrase donc il faudrait l'intégrer dans la boucle for d'en dessous
 def add_chunk_to_database(chunk):
   embedding = get_embeddings(sentences)[0]
   VECTOR_DB.append((chunk, embedding))
