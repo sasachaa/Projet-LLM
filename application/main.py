7d0fca4fe0ec12f747a144dd530c70ab3d8031ac
sasachaa <sarahchaab007@gmail.com>
2025-03-27 23:22:24 +0100

    newmain

--- application/main.py
+++ application/main.py
@@ -1,13 +1,17 @@
-from pdf2text2 import main
-from splitChunk import main
-from rag import main
+# main.py
+
+from pdf2text2 import main as pdf2txt_main
+from splitChunk import main as chunk_main
+from rag import main as rag_main
 
 pdf_file = "DL-Exemple.pdf"
-txt_path = main(pdf_file,20)
+
+# 1) Conversion PDF -> .txt
+txt_path = pdf2txt_main(pdf_file, 20)
 print(f"Fichier texte généré: {txt_path}")
 
-
-output_chunk_file = main(
+# 2) Découpe en chunks
+output_chunk_file = chunk_main(
     txt_input_path=txt_path,
     chunk_size=400,
     chunk_overlap=0,
@@ -16,4 +20,6 @@ output_chunk_file = main(
 )
 print("Fichier généré:", output_chunk_file)
 
-main(chunk_file=output_chunk_file)
+# 3) Lancement du RAG
+rag_main(chunk_file=output_chunk_file)
+
