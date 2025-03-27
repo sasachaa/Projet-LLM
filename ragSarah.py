d323eccae2a3ec12cdd7bc266952eed48267fb67
sasachaa <sarahchaab007@gmail.com>
2025-03-27 17:28:45 +0100

    push avant de partir

--- ragSarah.py
+++ ragSarah.py
@@ -47,7 +47,6 @@ def get_embeddings(sentences, model_name="sentence-transformers/all-MiniLM-L6-v2
 
 sentences = [dataset]
 
-print(sentences)
 
 def add_chunk_to_database(chunk):
   embedding = get_embeddings(sentences)[0]
