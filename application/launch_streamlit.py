fb6b4fe302b57dbdade87302554ad1ebcb61c2e7
sasachaa <sarahchaab007@gmail.com>
2025-04-01 16:32:32 +0200

    ffffd

--- application/launch_streamlit.py
+++ application/launch_streamlit.py
@@ -1,5 +1,6 @@
 from pyngrok import ngrok
 
 # Démarrer le tunnel sur le port 8501
+ngrok.set_auth_token("2uv56qqmgxaDZT25DpiqPrZBXqk_2soktLPAJbN6gMh4sZqkt")
 public_url = ngrok.connect(8501)
 print("Accédez à votre application via :", public_url)
